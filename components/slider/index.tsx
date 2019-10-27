cb533aff47e68cba66932f5c2361876d69b6907a
Yura <yura.kulinich.ua@gmail.com>
2019-10-27 13:20:11 +0200

    Fixed bug with first element of slider

--- components/slider/index.tsx
+++ components/slider/index.tsx
@@ -9,133 +9,143 @@ interface IProps {
   chooseExample: (exampleKey: KExample) => void
 }
 
-interface IScrollPos {
+interface ITime {
   start: number
   end: number
 }
 
-export const Slider: SFC<IProps> =
-  ({ choosenExampleKey, examples, chooseExample }) => {
-    const [startTime, setStartTime] = useState<number | null>(null)
-    const [requestId, setRequestId] = useState<number | null>(null)
-    const [scrollPos, setScrollPos] = useState<Nullable<IScrollPos>>({
-      start: null,
-      end: null
-    })
+interface IPos {
+  start: number
+  end: number
+}
 
-    const container = useRef<HTMLUListElement | null>(null)
+interface IState {
+  time: Nullable<ITime>
+  pos: Nullable<IPos>
+}
 
-    const TIME_DIFF = 200
-    const PASSIVE_SCALE = 0.9
+export const Slider: SFC<IProps> = ({ choosenExampleKey, examples, chooseExample }) => {
+  const TIME_DIFF = 200
+  const PASSIVE_SCALE = 0.9
+  
+  const [requestId, setRequestId] = useState<number | null>(null)
+  const [state, setState] = useState<IState | null>({
+    time: { start: null, end: null },
+    pos: { start: null, end: null }
+  })
 
-    function endTime(): number | null {
-      return startTime ? startTime + TIME_DIFF : null
-    }
+  const container = useRef<HTMLUListElement | null>(null)
 
-    function getCurrentPos(startPos: number, endPos: number): number {
-      const now = Date.now()
-      const posDiff = endPos - startPos
-      return now < endTime()
-        ? startPos + posDiff * (now - startTime) / TIME_DIFF
-        : endPos
-    }
+  function getCurrentPos(startPos: number, endPos: number): number {
+    const now = Date.now()
+    const posDiff = endPos - startPos
+    return now < state.time.end
+      ? startPos + posDiff * (now - state.time.start) / TIME_DIFF
+      : endPos
+  }
 
-    function newStartPos(now: number, startPos: number, endPos: number): number {
-      return now < endTime() ? getCurrentPos(startPos, endPos) : endPos
-    }
-
-    function updateAnimationData(scrollTop: number): void {
-      const now = Date.now()
-      setStartTime(now)
-      setScrollPos(({ end }) => ({
-        start: newStartPos(now, end, scrollTop),
+  function updateState(scrollTop: number): void {
+    const now = Date.now()
+    setState(({ pos: { start, end }}) => ({
+      time: {
+        start: now,
+        end: now + TIME_DIFF
+      },
+      pos: {
+        start: getCurrentPos(start, end),
         end: scrollTop
-      }))
-    }
-
-    function scrollSlides(e: React.UIEvent<HTMLUListElement>): void {
-      updateAnimationData(e.currentTarget.scrollTop)
-      if (!requestId) {
-        setRequestId(window.requestAnimationFrame(runAnimation))
       }
-    }
+    }))
+  }
 
-    function runAnimation(): void {
-      animate()
-
-      if (Date.now() >= endTime()) {
-        window.cancelAnimationFrame(requestId)
-        setRequestId(null)
-        return
-      }
-    
+  function scrollSlides(e: React.UIEvent<HTMLUListElement>): void {
+    updateState(e.currentTarget.scrollTop)
+    if (!requestId) {
       setRequestId(window.requestAnimationFrame(runAnimation))
     }
-
-    function getRotatedElement(elements: Element[], containerTop: number): [Element, number] | null {
-      const elIndex: number = elements.findIndex(el =>
-        el.getBoundingClientRect().top - containerTop < 0 
-        && el.getBoundingClientRect().top + el.clientHeight > containerTop
-      )
-      return elIndex !== -1 ? [elements[elIndex], elIndex] : null 
-    }
-
-    function updatedBottomElements(elements: Element[]): void {
-      elements.forEach(el => el.removeAttribute('style'))
-    }
-
-    function getScale(elTop: number, elHeight: number, containerTop: number, currentDelta: number, isChoosen: boolean): number {
-      const elBottom = elTop + elHeight
-      const coef = isChoosen ? 1 : PASSIVE_SCALE
-      return ((elBottom - containerTop + currentDelta) / elHeight) * coef
-    }
-
-    function isChoosen(elIndex): boolean {
-      return examples[elIndex][0] === choosenExampleKey
-    }
-
-    function animate(): void {
-      const currentContainer = container.current
-      if (!currentContainer) return
-    
-      const containerTop = currentContainer.getBoundingClientRect().top
-      const containerChildren = Array.from(currentContainer.children)
-
-      const currentPos = getCurrentPos(scrollPos.start, scrollPos.start)
-      const currentDelta = scrollPos.end - currentPos
-
-      const re = getRotatedElement(containerChildren, containerTop + currentDelta)
-      if (!re) return
-      const [rotatedElement, rotatedElementIndex] = re
-
-      const scaleVal = getScale(
-        rotatedElement.getBoundingClientRect().top,
-        rotatedElement.clientHeight,
-        containerTop,
-        currentDelta,
-        isChoosen(rotatedElementIndex)
-      )
-      rotatedElement.setAttribute('style', `transform: scale(${scaleVal > 0.01 ? scaleVal : 0.01})`)
-
-      updatedBottomElements(containerChildren.slice(rotatedElementIndex + 1))
-    }
-
-    return (
-      <ul
-        className={scss.examples}
-        onScroll={scrollSlides}
-        ref={container}
-      >
-        {
-          examples.map(example => (
-            <Example
-              example={example}
-              chooseExample={chooseExample}
-              choosenExampleKey={choosenExampleKey}
-              key={example[0]}
-            />
-          ))
-        }
-      </ul>
-    )
   }
+
+  function runAnimation(): void {
+    animate()
+
+    if (Date.now() >= state.time.end + TIME_DIFF) {
+      window.cancelAnimationFrame(requestId)
+      setRequestId(null)
+      return
+    }
+  
+    setRequestId(window.requestAnimationFrame(runAnimation))
+  }
+
+  function getScaledElement(elements: Element[], containerTop: number): [Element, number] | null {
+    const elIndex: number = elements.findIndex(el =>
+      el.getBoundingClientRect().top - containerTop < 0 
+      && el.getBoundingClientRect().top + el.clientHeight > containerTop
+    )
+    return elIndex !== -1 ? [elements[elIndex], elIndex] : null 
+  }
+
+  function getScale(
+    elTop: number, elHeight: number, containerTop: number, currentDelta: number, isChoosen: boolean
+  ): number {
+    const elBottom = elTop + elHeight
+    const coef = isChoosen ? 1 : PASSIVE_SCALE
+    return ((elBottom - containerTop + currentDelta) / elHeight) * coef
+  }
+
+  function isChoosen(elIndex): boolean {
+    return examples[elIndex][0] === choosenExampleKey
+  }
+
+  function updatedBottomElements(elements: Element[]): void {
+    elements.forEach(el => el.removeAttribute('style'))
+  }
+
+  function animate(): void {
+    const currentContainer = container.current
+    if (!currentContainer) return
+  
+    const containerTop = currentContainer.getBoundingClientRect().top
+    const containerChildren = Array.from(currentContainer.children)
+
+    const currentPos = getCurrentPos(state.pos.start, state.pos.end)
+    const currentDelta = state.pos.end - currentPos
+
+    const se = getScaledElement(containerChildren, containerTop + currentDelta)
+    if (!se) {
+      updatedBottomElements(containerChildren)
+      return
+    }
+  
+    const [scaledElement, scaledElementIndex] = se
+
+    const scaleVal = getScale(
+      scaledElement.getBoundingClientRect().top,
+      scaledElement.clientHeight,
+      containerTop,
+      currentDelta,
+      isChoosen(scaledElementIndex)
+    )
+
+    scaledElement.setAttribute('style', `transform: scale(${scaleVal > 0.01 ? scaleVal : 0.01})`)
+
+    updatedBottomElements(containerChildren.slice(scaledElementIndex + 1))
+  }
+
+  return (
+    <ul
+      className={scss.examples}
+      onScroll={scrollSlides}
+      ref={container}
+    >
+      {examples.map(example => (
+        <Example
+          example={example}
+          chooseExample={chooseExample}
+          choosenExampleKey={choosenExampleKey}
+          key={example[0]}
+        />
+      ))}
+    </ul>
+  )
+}
