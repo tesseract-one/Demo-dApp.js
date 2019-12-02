c72855740f50fabcf256cc9ec890ea40ad556ea2
Yura <yura.kulinich.ua@gmail.com>
2019-12-02 21:24:45 +0200

    Fixed build

--- next.config.js
+++ next.config.js
@@ -1,70 +1,73 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withSass = require('@zeit/next-sass')
+const withCSS = require('@zeit/next-css')
 const webpack = require('webpack')
 
-module.exports = withSass({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: "[local]___[hash:base64:5]",
-  },
-  sassLoaderOptions: {
-    data: `
-      @import './assets/styles/variables.scss';
-      @import './assets/styles/rubber.scss';
-    `
-  },
-  webpack: config => {
-    config.module.rules.forEach(rule => {
-      if (rule.test.toString().includes('.scss')) {
-        rule.rules = rule.use.map(useRule => {
-          if (typeof useRule === 'string') return { loader: useRule }
+module.exports = withCSS(
+    withSass({
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: "[local]___[hash:base64:5]",
+    },
+    sassLoaderOptions: {
+      data: `
+        @import './assets/styles/variables.scss';
+        @import './assets/styles/rubber.scss';
+      `
+    },
+    webpack: config => {
+      config.module.rules.forEach(rule => {
+        if (rule.test.toString().includes('.scss')) {
+          rule.rules = rule.use.map(useRule => {
+            if (typeof useRule === 'string') return { loader: useRule }
 
-          return useRule.loader.startsWith('css-loader')
-            ? {
-                oneOf: [
-                  {
-                    test: new RegExp('.scss$'),
-                    resourceQuery: /^\?raw$/,
-                    loader: useRule.loader,
-                    options: { ...useRule.options, modules: false }
-                  },
-                  {
-                    loader: useRule.loader,
-                    options: useRule.options
-                  }
-                ]
-              }
-            : useRule
-        })
+            return useRule.loader.startsWith('css-loader')
+              ? {
+                  oneOf: [
+                    {
+                      test: new RegExp('.scss$'),
+                      resourceQuery: /^\?raw$/,
+                      loader: useRule.loader,
+                      options: { ...useRule.options, modules: false }
+                    },
+                    {
+                      loader: useRule.loader,
+                      options: useRule.options
+                    }
+                  ]
+                }
+              : useRule
+          })
 
-        delete rule.use
-      }
-    })
-    config.module.rules.push({
-      test: /\.txt$/i,
-      use: 'raw-loader'
-    })
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|eot|ttf|woff|woff2)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          limit: 100000,
-          name: '[name].[ext]'
+          delete rule.use
         }
-      }
-    })
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: '@svgr/webpack'
-    })
-    config.plugins.push(
-      new webpack.ContextReplacementPlugin(
-        new RegExp('/highlight.js/lib/languages$/'),
-        new RegExp(`^./(javascript)$`)
+      })
+      config.module.rules.push({
+        test: /\.txt$/i,
+        use: 'raw-loader'
+      })
+      config.module.rules.push({
+        test: /\.(png|jpe?g|gif|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000,
+            name: '[name].[ext]'
+          }
+        }
+      })
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: '@svgr/webpack'
+      })
+      config.plugins.push(
+        new webpack.ContextReplacementPlugin(
+          new RegExp('/highlight.js/lib/languages$/'),
+          new RegExp(`^./(javascript)$`)
+        )
       )
-    )
-    return config
-  }
-})
+      return config
+    }
+  })
+)
